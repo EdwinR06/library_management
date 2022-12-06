4f04d94ec81cbefe9184e4d02913abe75fc841af
Edwin Ryerson <eryerson06@gmail.com>
2022-12-05 20:33:15 -0700

    Create Patron Constructor

--- Patron.java
+++ Patron.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 public class Patron extends User {
     private ArrayList<CheckoutList> checkoutHistory;
 
+    public Patron(String username, String password) {
+        super(username, password);
+        this.checkoutHistory = new ArrayList<CheckoutList>();
+    }
+
     public void setCheckoutHistory(ArrayList<CheckoutList> checkoutHistory) {
         this.checkoutHistory = checkoutHistory;
     }
